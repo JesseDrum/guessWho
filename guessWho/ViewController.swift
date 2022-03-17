bb88f779cb754aef7b3a5d393034425f597f0814
JesseDrum <ynwa285@gmail.com>
2022-03-17 09:55:33 -0400

    beta

--- guessWho/ViewController.swift
+++ guessWho/ViewController.swift
@@ -18,14 +18,15 @@ class ViewController: UIViewController {
     
 
     var teachers = [
-        ["Name": "Mr. Walter", "Third Floor": true, "Female": false, "Math Teacher": true, "English Teacher": false, "Science Teacher": false, "History Teacher": false, "Language Teacher": false],
-        ["Name": "Ms. Laskaris", "Third Floor": true, "Female": true, "Math Teacher": true, "English Teacher": false, "Science Teacher": false, "History Teacher": false, "Language Teacher": false]
+        ["Name": "Mr. Walter", "Third Floor": true, "Female": false, "Math Teacher": true, "English Teacher": false, "Science Teacher": false, "History Teacher": false, "Language Teacher": true],
+        ["Name": "Ms. Laskaris", "Third Floor": true, "Female": true, "Math Teacher": true, "English Teacher": false, "Science Teacher": false, "History Teacher": false, "Language Teacher": false],
+        ["Name": "Sabrina", "Third Floor": true, "Female": true, "Math Teacher": true, "English Teacher": false, "Science Teacher": false, "History Teacher": true, "Language Teacher": true]
     ]
      
     
     
     override func viewDidLoad() {
-        <#code#>
+        
     }
     
     
@@ -36,10 +37,11 @@ class ViewController: UIViewController {
         
         // remove any that dont have the same answer
         
-        for i in stride(from: teachers.count, through: 0, by: -1) {
+        
+        for i in stride(from: teachers.count-1, through: 0, by: -1) {
             let teacherAnswer = teachers[i][property] as! Bool
             
-            if teacherAnswer != answer
+            if answer != teacherAnswer
             {
                 teachers.remove(at: i)
             }
@@ -52,14 +54,41 @@ class ViewController: UIViewController {
         
     }
     
-    
+    var count = 0
     @IBAction func yesButton(_ sender: Any) {
-   
+        count += 1
+        
+        if count == 1 {
+            check(answer: true, property: "Language Teacher")
+        }
+        if count == 2 {
+            check(answer: true, property: "History Teacher")
+        }
+        if count == 3 {
+            check(answer: true, property: "Science Teacher")
+        }
+        if count == 4 {
+            check(answer: true, property: "English Teacher")
+        }
+        if count == 5 {
+            check(answer: true, property: "Math Teacher")
+        }
+        if count == 6 {
+            check(answer: true, property: "Female")
+        }
+        if count == 7 {
+            check(answer: true, property: "Third Floor")
+        }
+        
+        
+        
         
     }
     
     @IBAction func noButton(_ sender: Any) {
   
+        
+        
         
     }
     
