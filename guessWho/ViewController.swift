e49a46ebec84b432b0e102c2bf2f7046caea03d2
JesseDrum <ynwa285@gmail.com>
2022-03-22 10:55:48 -0400

    save

--- guessWho/ViewController.swift
+++ guessWho/ViewController.swift
@@ -6,15 +6,21 @@ class ViewController: UIViewController {
     
     var count = 0
 
-//
-//    @objc func noClick(_sender: Any)
-//    {
-//
-//    }
-//    @objc func yesClick(_sender: Any)
-//    {
-//
-//    }
+
+  
+    
+    var teachers = [
+        ["Name": "Mr. Walter", "Third Floor": true, "Female": false, "Math Teacher": true, "English Teacher": false, "Science Teacher": false, "History Teacher": false, "Language Teacher": true],
+        ["Name": "Ms. Laskaris", "Third Floor": true, "Female": true, "Math Teacher": true, "English Teacher": false, "Science Teacher": false, "History Teacher": false, "Language Teacher": false],
+        ["Name": "Ms. Grady", "Third Floor": false, "Female": true, "Math Teacher": false, "English Teacher": true, "Science Teacher": false, "History Teacher": false, "Language Teacher": false]
+    ]
+     
+  
+    
+
+
+    
+    
     
     
     
@@ -35,50 +41,34 @@ class ViewController: UIViewController {
     var properties = ["Female", "Math Teacher","English Teacher", "Science Teacher", "History Teacher", "Language Teacher","Second Floor", "Third Floor", ]
 
 
-    var teachers = [
-        ["Name": "Mr. Walter", "Third Floor": true, "Female": false, "Math Teacher": true, "English Teacher": false, "Science Teacher": false, "History Teacher": false, "Language Teacher": true],
-        ["Name": "Ms. Laskaris", "Third Floor": true, "Female": true, "Math Teacher": true, "English Teacher": false, "Science Teacher": false, "History Teacher": false, "Language Teacher": false],
-        ["Name": "Sabrina", "Third Floor": true, "Female": true, "Math Teacher": false, "English Teacher": false, "Science Teacher": false, "History Teacher": true, "Language Teacher": true]
-    ]
-     
   
     
     override func viewDidLoad() {
         questionsForGAME()
-//        loadQuestion(question: game.first!)
         loadQuestion(question: count)
 
         
     }
     
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "one" {
+            let destination = segue.destination as! UserinputView
+            destination.games = teachers
+            
+        }
+      
+    }
     
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    func questionsForGAME(){
-        game.append(Question(text: "Is your teacher a female?"));
+    func questionsForGAME()
+    {
         game.append(Question(text: "Are they a math teacher?"));
+        game.append(Question(text: "Are they on the third floor"));
         game.append(Question(text: "Is your teacher a female?"));
         game.append(Question(text: "Is your teacher a female?"));
         game.append(Question(text: "Is your teacher a female?"));
         game.append(Question(text: "Is your teacher a female?"));
         game.append(Question(text: "Is your teacher a female?"));
-
-
-    
-    
-}
+    }
     
     
     
@@ -103,12 +93,8 @@ class ViewController: UIViewController {
     
     func check(answer: Bool, property: String)
     {
-        
-        // loop through all teachers in arrray of dictionarys
-        
-        // remove any that dont have the same answer
-//        myLabel.text = game[count+1]
-        
+   
+        print("Before filter \(teachers.count)")
         for i in stride(from: teachers.count-1, through: 0, by: -1) {
             let teacherAnswer = teachers[i][property] as! Bool
             
@@ -118,64 +104,93 @@ class ViewController: UIViewController {
             }
             
         }
+        print("After filter \(teachers.count)")
         if teachers.count == 1
         {
             print("It's your teacher \(teachers[0]["Name"] as! String)")
             myLabel.text = "It's your teacher \(teachers[0]["Name"] as! String)?"
             
             
-//            let yesButton = UIButton(frame: CGRect(x: 50, y: 590, width: 100, height: 50))
-//            yesButton.setTitle("YesButton", for: .normal)
-//            self.view.addSubview(yesButton)
-//            yesButton.addTarget(self, action: #selector(yesClick), for: .touchUpInside)
+            
+        }
+        
+        
 //
-//            let noButton = UIButton(frame: CGRect(x: 250, y: 590, width: 100, height: 50))
-//            noButton.setTitle("NoButton", for: .normal)
-//            self.view.addSubview(noButton)
-//            noButton.addTarget(self, action: #selector(noClick), for: .touchUpInside)
-        }
-        
-        
-       
-        if count > properties.count-1
-        {
-            count = 0
-
-        }
-        
+//        if count > properties.count-1
+//        {
+//            count = 0
+//
+//        }
+//
     }
     
     @IBAction func yesButton(_ sender: Any) {
         check(answer: true, property: properties[count])
         count += 1
-        if count == properties.count
-        {
-            count = 0
-        }
+
         loadQuestion(question: count)
         
-        if teachers.count == 1{
-            
+        if teachers.count < 1
+        {
+            YesAlerts()
         }
         
         
     }
 
+    
     @IBAction func noButton(_ sender: Any) {
         check(answer: false, property: properties[count])
         count += 1
-        if count == properties.count
-        {
-            count = 0
-        }
         loadQuestion(question: count)
         
-        if teachers.count == 1{
-         
+      if teachers.count < 1
+      {
+//            performSegue(withIdentifier: "one", sender: self)
+            NoAlerts()
+      }
+
         }
         
+        func YesAlerts()
+        {
+            let alert = UIAlertController(title: "Title", message: "Please Select an Option", preferredStyle: .actionSheet)
+            alert.addAction(UIAlertAction(title: "Approve", style: .default, handler: { (_) in
+                print("PLAY AGAIN")
+            }))
+          
+        }
         
-        
+        func NoAlerts()
+        {
+            let alertController = UIAlertController(title: "Add new tag", message: nil, preferredStyle: .alert)
+                 let confirmAction = UIAlertAction(title: "Add", style: .default) { (_) in
+                     if let txtField = alertController.textFields?.first, let text = txtField.text {
+                         // operations
+                         print("Text==>" + text)
+                     }
+                 }
+                 let cancelAction = UIAlertAction(title: "Cancel", style: .cancel) { (_) in }
+                 alertController.addTextField { (textField) in
+                     textField.placeholder = "Tag"
+                 }
+                 alertController.addAction(confirmAction)
+                 alertController.addAction(cancelAction)
+                 self.present(alertController, animated: true, completion: nil)
+        }
+    
+    
+    func presentAlertController() {
+        let alertController = UIAlertController(title: "Login", message: nil, preferredStyle: .alert)
+        self.present(alertController, animated: true)
+    }
+//
+//    override func viewDidAppear(_ animated: Bool) {
+//        super.viewDidAppear(animated)
+//        self.presentAlertController()
+//    }
+//
+    
     }
     
     
@@ -188,7 +203,7 @@ class ViewController: UIViewController {
     
     
     
-}
+
 
 struct Question{
     let text: String
